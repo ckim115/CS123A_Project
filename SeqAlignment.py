aebdf3232a9f792ae1b0f771477ef27e2ac59710
ckim115 <christina.hayoung@gmail.com>
2024-11-18 13:10:47 -0800

    clip max input for np.exp

--- SeqAlignment.py
+++ SeqAlignment.py
@@ -30,16 +30,20 @@ class Score:
     
     # This function calculates the optimal alignment score for two sequences using the Needleman-Wunsch algorithm.
     def compute_pairwise_distances(self):
+        print("beginning pairwise alignment")
         num_sequences = len(self.sequences)
         distance_matrix = np.zeros((num_sequences, num_sequences))
 
         # Calculate pairwise distances
         for i in range(num_sequences):
             for j in range(i + 1, num_sequences):
+                print("getting alignment_score")
                 alignment_score = self.needleman_wunsch(self.sequences[i], self.sequences[j])
                 # This part of algorithm is . there are several ways to calculate distance.
                 # Scaled Distance Formula used here. 0 means two sequent are very close
                 # This formula asures that distance never get infinit or negetive value,always between 0 to 1
+                max_value = np.log(np.finfo(np.float64).max) - 1
+                alignment_score = np.clip(alignment_score, -max_value, max_value)
                 distance = 1 / (1 + np.exp(alignment_score))
                 distance_matrix[i][j] = distance
                 distance_matrix[j][i] = distance
